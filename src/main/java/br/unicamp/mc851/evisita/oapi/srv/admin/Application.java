cad6bb957bc3372c4ca4fb2d8ab6364a4aa2e4d9
Natan Rodrigues de Oliveira <natanrodriguesdeoliveira@gmail.com>
2021-06-15 22:08:31 -0300

    first commit

---
+++ src/main/java/br/unicamp/mc851/evisita/oapi/srv/admin/Application.java
@@ -0,0 +1,13 @@
+package br.unicamp.mc851.evisita.oapi.srv.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/unicamp/mc851/evisita/oapi/srv/admin/ApplicationTests.java
@@ -0,0 +1,15 @@
+package br.unicamp.mc851.evisita.oapi.srv.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
